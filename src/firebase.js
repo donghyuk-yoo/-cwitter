10db0d3d4cb9e5a32028da279000631c45f8e719
YOODONGHYUK <ydh8485@gmail.com>
2021-01-05 00:19:22 +0900

    Revert "update"
    This reverts commit b5cae61f62c723ccf00b53b0149984ec1d2e82c7.

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTO_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyDz-Dv3H-hq7PInr0IaHKTH9B0c4tIF2kI",
+    authDomain: "cwitter-852f2.firebaseapp.com",
+    projectId: "cwitter-852f2",
+    storageBucket: "cwitter-852f2.appspot.com",
+    messagingSenderId: "367208070762",
+    appId: "1:367208070762:web:27a755fb4327938d79d413"
   };
 
   export default firebase.initializeApp(firebaseConfig);
